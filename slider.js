c24b31137b65c285c08a095c2c290aa5550ae623
phimhayplus <phimhayplus.net@gmail.com>
2017-05-24 10:01:36 +0700

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,219 @@
+.mbtslides-title {
+font-family:Roboto;
+font-weight:400;
+font-size:18px!important;
+border-bottom:2px solid #f90;
+color:#242729;
+width:300px
+}
+
+.flexslider {
+width:300px;
+margin:0;
+border:0
+}
+
+.flexslider .slides {
+list-style-type:none;
+margin:0 auto 10px;
+padding:0
+}
+
+.flexslider li {
+color:#666;
+font-family:helvetica;
+font-size:12px;
+overflow:hidden;
+position:relative;
+padding-top:10px;
+line-height:1.6em
+}
+
+.flexslider .flex-control-nav {
+list-style:none outside none;
+position:absolute;
+top:-45px;
+right:5px;
+text-align:right;
+height:10px
+}
+
+.flexslider .flex-control-nav li {
+display:inline-block;
+margin:0 2px
+}
+
+.flexslider .flex-control-nav a {
+text-indent:9999px;
+display:block;
+width:10px;
+cursor:pointer;
+height:10px;
+background-color:#000;
+border:1px solid #000;
+border-radius:100%!important
+}
+
+.flexslider .flex-control-nav a.flex-active {
+background-color:#f90;
+border:1px solid #f90;
+box-shadow: none;
+}
+
+.flexslider .flex-div {
+margin:0 auto 20px;
+display:block;
+border-bottom:1px solid #eee;
+padding-bottom:20px!important;
+position:relative
+}
+
+.flexslider li a {
+color:#0080ff;
+text-decoration:none
+}
+
+.flexslider i {
+color:#999;
+padding-right:5px
+}
+
+.flexslider .iline {
+line-height:2em;
+margin-top:3px
+}
+
+.flexslider .icontent {
+line-height:1.5em;
+margin-top:5px
+}
+
+.flexslider div span {
+margin:0 5px 0 0;
+display:inline-block;
+font-weight:400
+}
+
+.flexslider .mbttitle {
+font-family:Roboto;
+font-size:13px;
+color:#666;
+font-weight:400;
+text-decoration:none
+}
+
+.flexslider .mbttitle:hover,.flexslider .itotal a:hover {
+color:#333;
+text-decoration:none
+}
+
+.flexslider .iedit a {
+text-decoration:none;
+color:#999;
+cursor:pointer
+}
+
+.flexslider .iedit:before,.flexslider .iauthor:before,.flexslider .itag:before,.flexslider .icomments:before,.flexslider .idate:before,.flexslider .itotal span:before {
+font-family:fontAwesome;
+position:relative;
+padding-right:8px;
+color:#999
+}
+
+.flexslider .iauthorpic {
+width:15px!important;
+height:15px!important;
+border-radius:50%;
+float:none;
+display:inline-block!important;
+margin:0;
+padding-right:3px;
+position:relative;
+top:3px
+}
+
+.flexslider .iFeatured {
+overflow:hidden;
+position:relative;
+float:left;
+margin:0 5px 10px 0;
+padding:0
+}
+
+.flexslider .iFeatured a {
+background:none;
+padding:0;
+display:block;
+border:1px solid #eee
+}
+
+.flexslider .iFeatured img {
+width:110px!important;
+height:65px!important;
+-moz-transition:all .3s;
+-webkit-transition:all .3s;
+transition:all .3s;
+border-radius:2px
+}
+
+.flexslider .iFeatured:hover img {
+opacity:1;
+-moz-transform:scale(1.4);
+-webkit-transform:scale(1.4);
+transform:scale(1.4)
+}
+
+.flexslider .icomments a {
+color:#0080ff;
+font-family:arial;
+font-size:12px
+}
+
+.flexslider .icomments a:hover {
+text-decoration:underline
+}
+
+.flexslider .icomments:before {
+content:'\f086';
+padding:0 3px 0 7px;
+color:#f90
+}
+
+.flexslider .idate {
+position:absolute;
+left:0;
+top:-7px;
+background:#f90;
+color:#fff;
+padding:3px 4px;
+font-family:Roboto;
+font-size:11px;
+border:1px solid #f90
+}
+
+.flexslider .idate div {
+width:auto;
+line-height:1.6em;
+text-align:center
+}
+
+.flexslider .idate:after {
+content:' ';
+position:absolute;
+z-index:-9999px;
+width:0;
+height:0;
+left:29px;
+top:-1px;
+border-width:4px 6px;
+border-style:solid;
+border-color:transparent transparent #f90 #f90
+}
+
+.flexslider .flex-div-v {
+margin:10px;
+display:block;
+border-bottom:1px solid #eee;
+padding-bottom:10px!important;
+position:relative
+}
